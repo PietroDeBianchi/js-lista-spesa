ba95dcb09f91132d99ad7f96c6c0cf5a08772243
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-03-01 17:37:14 +0100

    lista spesa

---
+++ main.js
@@ -0,0 +1,18 @@
+// Get the necessary HTML element
+const listaSpesa = document.getElementById("listaSpesa");
+
+// Use a while loop to continuously prompt for new items
+while (true) {
+  // Prompt the user to enter a new item
+  const newItem = prompt("Aggiungi un elemento alla lista:");
+
+  // Exit the loop if the user clicks "Cancel" or enters an empty string
+  if (newItem === null || newItem.trim() === "") {
+    break;
+  }
+
+  // Create a new <li> element and append it to the <ul> list
+  const li = document.createElement("li");
+  li.textContent = newItem;
+  listaSpesa.appendChild(li);
+}
