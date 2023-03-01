44ab68c24a1a1ea599943b433e12a9c56a1c6a07
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-03-01 17:54:42 +0100

    clean-code-line10: newItem = null removed not nec.

--- main.js
+++ main.js
@@ -7,12 +7,12 @@ while (true) {
   const newItem = prompt("Aggiungi un elemento alla lista:");
 
   // Exit the loop if the user clicks "Cancel" or enters an empty string
-  if (newItem === null || newItem.trim() === "") {
+  if (newItem.trim() === "") {
     break;
   }
-
+  
   // Create a new <li> element and append it to the <ul> list
   const li = document.createElement("li");
   li.textContent = newItem;
   listaSpesa.appendChild(li);
-}
+};
